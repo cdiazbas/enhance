5df74852779fd7dcb0048a31b91e0d99c7eda764
cdiazbas <carloskdb@gmail.com>
2019-11-27 14:56:25 +0100

    it works with new version of keras (up to 2.3.1)

--- enhance.py
+++ enhance.py
@@ -1,35 +1,35 @@
+import warnings
+# To deactivate future warnings:
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 import numpy as np
 import platform
 import os
 import time
 import argparse
 from astropy.io import fits
+import tensorflow as tf
+import keras as krs
+import keras.backend.tensorflow_backend as ktf
+import models as nn_model
+
 
 # To deactivate warnings: https://github.com/tensorflow/tensorflow/issues/7778
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+tf.logging.set_verbosity(tf.logging.ERROR)
 
-if (platform.node() == 'viga'):
-    os.environ["THEANO_FLAGS"] = "mode=FAST_RUN,device=cpu,floatX=float32"
 
+# Using TensorFlow backend
 os.environ["KERAS_BACKEND"] = "tensorflow"
 
-if (platform.node() != 'viga'):
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-import tensorflow as tf
-import keras.backend.tensorflow_backend as ktf
-
-import models as nn_model
+print('tensorflow version:',tf.__version__)
+print('keras version:',krs.__version__)
 
 class enhance(object):
 
     def __init__(self, inputFile, depth, model, activation, ntype, output):
 
-# Only allocate needed memory
-        config = tf.ConfigProto()
-        config.gpu_options.allow_growth=True
-        session = tf.Session(config=config)
-        ktf.set_session(session)
         self.hdu = fits.open(inputFile)
         #Autofix broken header files according to fits standard
         self.hdu.verify('silentfix')
@@ -55,12 +55,6 @@ class enhance(object):
         if (self.network_type == 'encdec'):
             self.model = nn_model.encdec(self.ny, self.nx, 0.0, self.depth, n_filters=64)
 
-        # if (self.network_type == 'encdec_reflect'):
-        #     self.model = nn_model.encdec_reflect(self.nx, self.ny, 0.0, self.depth, n_filters=64)
-
-        # if (self.network_type == 'keepsize_zero'):
-        #     self.model = nn_model.keepsize_zero(self.nx, self.ny, 0.0, self.depth)
-
         if (self.network_type == 'keepsize'):
             self.model = nn_model.keepsize(self.ny, self.nx, 0.0, self.depth,n_filters=64, l2_reg=1e-7)
 
@@ -68,7 +62,7 @@ class enhance(object):
         self.model.load_weights("network/{0}_weights.hdf5".format(self.ntype))
 
 
-    def predict(self):
+    def predict(self,plot_option=False):
         print("Predicting validation data...")
 
         input_validation = np.zeros((1,self.ny,self.nx,1), dtype='float32')
@@ -117,12 +111,26 @@ class enhance(object):
             print('Overwriting...')
         hdu.writeto('{0}'.format(self.output))
 
-        # import matplotlib.pyplot as plt
-        # plt.imshow(out[0,:,:,0])
-        # plt.savefig('hmi.pdf')
+        if plot_option is True:
+            import matplotlib.pyplot as plt
+            plt.subplot(121)
+            plt.imshow(self.image,cmap='gray',origin='lower',vmin=self.image.min(),vmax=self.image.max())
+            plt.subplot(122)
+            plt.imshow(out[0,:,:,0],cmap='gray',origin='lower',vmin=self.image.min(),vmax=self.image.max())
+            plt.savefig('hmi_test.pdf', bbox_inches='tight')
 
 
 if (__name__ == '__main__'):
+
+    """
+    Using Enhance for prediction:
+    =============================
+
+    python enhance.py -i samples/hmi.fits -t intensity -o output/hmi_enhanced.fits
+
+    python enhance.py -i samples/blos.fits -t blos -o output/blos_enhanced.fits
+
+    """
 
     parser = argparse.ArgumentParser(description='Prediction')
     parser.add_argument('-i','--input', help='input')
@@ -130,7 +138,6 @@ if (__name__ == '__main__'):
     parser.add_argument('-d','--depth', help='depth', default=5)
     parser.add_argument('-m','--model', help='model', choices=['encdec', 'encdec_reflect', 'keepsize_zero', 'keepsize'], default='keepsize')
     parser.add_argument('-c','--activation', help='Activation', choices=['relu', 'elu'], default='relu')
-    # parser.add_argument('-a','--action', help='action', choices=['cube', 'movie'], default='cube')
     parser.add_argument('-t','--type', help='type', choices=['intensity', 'blos'], default='intensity')
     parsed = vars(parser.parse_args())
 
@@ -142,11 +149,10 @@ if (__name__ == '__main__'):
     out = enhance('{0}'.format(parsed['input']), depth=int(parsed['depth']), model=parsed['model'], activation=parsed['activation'],ntype=parsed['type'], output=parsed['out'])
     #out.define_network(image=imgs)
     out.define_network()
-    out.predict()
+    out.predict(plot_option=False)
+    
     # To avoid the TF_DeleteStatus message:
     # https://github.com/tensorflow/tensorflow/issues/3388
     ktf.clear_session()
+ 
 
-    # python enhance.py -i samples/hmi.fits -t intensity -o output/hmi_enhanced.fits
-
-    # python enhance.py -i samples/blos.fits -t blos -o output/blos_enhanced.fits
